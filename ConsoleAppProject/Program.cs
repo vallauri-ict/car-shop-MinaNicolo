adcdda6a4ce358eeb8cff1d7cfdb97db822472c6
MinaNicolo <61827162+MinaNicolo@users.noreply.github.com>
2020-05-10 12:25:43 +0200

    Update

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleAppProject
 {
     class Program
     {
-        public static string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=CarShop.accdb";
+        public static string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Veicoli.accdb";
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t----- SALONE VEICOLI USATI E NUOVI -----\n");
@@ -26,6 +26,7 @@ namespace ConsoleAppProject
             SerializableBindingList<veicolo> listaVeicoli = new SerializableBindingList<veicolo>();
 
             string scelta;
+            string tabella;
 
             do
             {
@@ -38,7 +39,8 @@ namespace ConsoleAppProject
                     case "H":
                         Console.WriteLine("\n" + File.ReadAllText(@".\Comandi.txt") + "\n");
                         break;
-                    case "C": CreateTable(); break;
+                    case "C": Console.Write("Creare tabella Auto o Moto? [auto/moto]: ");
+                        tabella = Console.ReadLine().Trim().ToLower(); CreateTable(tabella); break;
                     case "A":
                         Console.WriteLine("\n\t\t\tAGGIUNGI VEICOLO\n");
                         Console.Write("Auto o Moto? [A/M]: ");
@@ -53,8 +55,11 @@ namespace ConsoleAppProject
                         AddNewVehicle(v, listaVeicoli);
                        
                         break;
-                    case "LS": CarList();break;
-
+                    case "LS": Console.Write("\nInserisci il nome della tabella da visualizzare [auto/moto]: ");
+                            tabella = Console.ReadLine().Trim().ToLower(); CarList(tabella);break;
+                    case "D":
+                        Console.Write("\nInserisci il nome della tabella da eliminare [auto/moto]: ");
+                        tabella = Console.ReadLine().Trim().ToLower(); DropTable(tabella); break;
                     default:
                         Console.WriteLine("Comando non disponibile!");
                         Console.ReadKey();
@@ -64,6 +69,26 @@ namespace ConsoleAppProject
 
             } while (scelta != "X");
             
+        }
+
+        private static void DropTable(string tabella)
+        {
+            if(connStr!=null)
+            {
+                OleDbConnection con = new OleDbConnection(connStr);
+                using (con)
+                {
+                    con.Open();
+
+                    OleDbCommand cmd = new OleDbCommand();
+                    cmd.Connection = con;
+                    string command = $"DROP TABLE {tabella}";
+                    cmd.CommandText = command;
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine($"\nTable {tabella} removed");
+                    System.Threading.Thread.Sleep(3000);
+                }
+            }
         }
 
         private static void AddNewVehicle(veicolo v, SerializableBindingList<veicolo> lista)
@@ -94,15 +119,15 @@ namespace ConsoleAppProject
                         if (v is auto)
                         {
                             (v as auto).NumairBag = Convert.ToInt32(SetFields("Inserisci numero di airbag: "));
-                            query = "INSERT INTO cars(marca, modello, cilindrata, potenzaKw," +
-                                "immatricolazione, kmPercorsi, colore, isUsato, isKmZero, numAirBag) VALUES(@marca, @modello," +
+                            query = "INSERT INTO auto(marca, modello, cilindrata, potenzaKw," +
+                                "immatr, kmPercorsi, colore, isUsato, isKmZero, nAirBag) VALUES(@marca, @modello," +
                                 "@cilindrata, @potenzaKw, @immatr, @kmPercorsi, @colore, @isUsato, @isKmZero, @nAirBag)";
                         }
                         else
                         {
                             (v as moto).MarcaSella = SetFields("Inserisci la marca della sella: ");
-                            query = "INSERT INTO cars(marca, modello, cilindrata, potenzaKw," +
-                                "immatricolazione, kmPercorsi, colore, isUsato, isKmZero, marcaSella) VALUES(@marca, @modello," +
+                            query = "INSERT INTO moto(marca, modello, cilindrata, potenzaKw," +
+                                "immatr, kmPercorsi, colore, isUsato, isKmZero, mSella) VALUES(@marca, @modello," +
                                 "@cilindrata, @potenzaKw, @immatr, @kmPercorsi, @colore, @isUsato, @isKmZero, @mSella)";
                         }
 
@@ -130,7 +155,7 @@ namespace ConsoleAppProject
             cmd.Parameters.Add(new OleDbParameter("@marca", OleDbType.VarChar, 255)).Value = v.Marca;
             cmd.Parameters.Add("@modello", OleDbType.VarChar, 255).Value = v.Modello;
             cmd.Parameters.Add("@cilindrata", OleDbType.Integer).Value = v.Cilindrata;
-            cmd.Parameters.Add("@potenzaKw", OleDbType.Double).Value = v.PotenzaKw;
+            cmd.Parameters.Add("@potenzaKw", OleDbType.Integer).Value = v.PotenzaKw;
             cmd.Parameters.Add("@immatr", OleDbType.Date).Value = v.Immatricolazione;
             cmd.Parameters.Add("@kmPercorsi", OleDbType.Integer).Value = v.KmPercorsi;
             cmd.Parameters.Add("@colore", OleDbType.VarChar, 255).Value = v.Colore;
@@ -158,7 +183,7 @@ namespace ConsoleAppProject
                 return r;
         }
 
-        private static void CarList()
+        private static void CarList(string tabella)
         {
             if (connStr != null)
             {
@@ -167,20 +192,33 @@ namespace ConsoleAppProject
                 {
                     connection.Open();
 
-                    OleDbCommand command = new OleDbCommand("SELECT * FROM cars", connection);
+                    OleDbCommand command = new OleDbCommand($"SELECT * FROM {tabella}", connection);
 
                     OleDbDataReader rdr = command.ExecuteReader();
-
+                   
                     if (rdr.HasRows)
                     {
                         Console.WriteLine("\n");
                         while (rdr.Read())
                         {
-                            Console.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}|{9}",
-                                rdr.GetInt32(0), rdr.GetString(1),rdr.GetInt32(2),
-                                rdr.GetInt32(3), rdr.GetString(4), rdr.GetInt32(5),
-                                rdr.GetInt32(6), rdr.GetString(7), rdr.GetInt32(8),
-                                rdr.GetInt32(9));
+                            if(tabella=="auto")
+                                Console.WriteLine("ID: {0}| Marca: {1}| Modello: {2}|" +
+                                    " Cilindrata: {3}| Potenza[Kw]: {4}| Immatricolazione: {5}|" +
+                                    " Km percorsi: {6}| Colore: {7}| Usato: {8}| Km zero: {9}|" +
+                                    " Numero airbag: {10}",
+                                    rdr.GetInt32(0), rdr.GetString(1),rdr.GetString(2),
+                                    rdr.GetInt32(3), rdr.GetInt32(4), rdr.GetDateTime(5).ToShortDateString(),
+                                    rdr.GetInt32(6), rdr.GetString(7), rdr.GetBoolean(8),
+                                    rdr.GetBoolean(9), rdr.GetInt32(10));
+                            else
+                                Console.WriteLine("ID: {0}| Marca: {1}| Modello: {2}|" +
+                                    " Cilindrata: {3}| Potenza[Kw]: {4}| Immatricolazione: {5}|" +
+                                    " Km percorsi: {6}| Colore: {7}| Usato: {8}| Km zero: {9}|" +
+                                    " Marca sella: {10}",
+                                   rdr.GetInt32(0), rdr.GetString(1), rdr.GetString(2),
+                                   rdr.GetInt32(3), rdr.GetInt32(4), rdr.GetDateTime(5).ToShortDateString(),
+                                   rdr.GetInt32(6), rdr.GetString(7), rdr.GetBoolean(8),
+                                   rdr.GetBoolean(9), rdr.GetString(10));
                         }
                     }
                     else
@@ -194,7 +232,7 @@ namespace ConsoleAppProject
             }
         }
 
-        private static void CreateTable()
+        private static void CreateTable(string tabella)
         {
             if (connStr != null)
             {
@@ -212,20 +250,36 @@ namespace ConsoleAppProject
                     //type BIT in the table = boolean
                     try
                     {
-                        cmd.CommandText = @"CREATE TABLE cars(
+                        if (tabella == "auto")
+                        {
+                            cmd.CommandText = @"CREATE TABLE auto(
                                             id int identity(1,1) NOT NULL PRIMARY KEY,
                                             marca VARCHAR(255) NOT NULL,
                                             modello VARCHAR(255) NOT NULL,
-                                            cilindrata INT,
-                                            potenzaKw DOUBLE,
-                                            immatr DATE,
-                                            kmPercorsi INT,
+                                            cilindrata INT NOT NULL,
+                                            potenzaKw INT NOT NULL,
+                                            immatr DATE NOT NULL,
+                                            kmPercorsi INT NOT NULL,
                                             colore VARCHAR(255) NOT NULL,
-                                            isUsato BIT,
-                                            isKmZero BIT,
-                                            nAirBag INT,
-                                            mSella VARCHAR(255)
-                                          )";
+                                            isUsato BIT NOT NULL CHEK,
+                                            isKmZero BIT NOT NULL CHEK,
+                                            nAirBag INT NOT NULL)";
+                        }
+                        else
+                        {
+                            cmd.CommandText = @"CREATE TABLE moto(
+                                                id int identity(1,1) NOT NULL PRIMARY KEY,
+                                                marca VARCHAR(255) NOT NULL,
+                                                modello VARCHAR(255) NOT NULL,
+                                                cilindrata INT NOT NULL,
+                                                potenzaKw INT NOT NULL,
+                                                immatr DATE NOT NULL,
+                                                kmPercorsi INT NOT NULL,
+                                                colore VARCHAR(255) NOT NULL,
+                                                isUsato BIT NOT NULL CHECK,
+                                                isKmZero BIT NOT NULL CHEK,
+                                                mSella VARCHAR(255) NOT NULL)";
+                        }
                         cmd.ExecuteNonQuery();
                     }
                     catch (OleDbException exc)
@@ -233,7 +287,7 @@ namespace ConsoleAppProject
                         Console.WriteLine("\n\n" + exc.Message);
                         System.Threading.Thread.Sleep(2000);
                         return;
-                    }                    
+                    }
                     Console.WriteLine("\n\nCars created!");
                     System.Threading.Thread.Sleep(3000);
                 }
